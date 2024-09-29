a7c8fb417b307f15f1a36af74642fa498c6347a3
Juan Luna Ramirez <8217959+lunajuan@users.noreply.github.com>
2024-09-29 10:07:47 -0700

    add ability to upload product

---
+++ scripts/upload-products.js
@@ -0,0 +1,74 @@
+import fs from "fs";
+import path from "path";
+import { PrismaClient } from "@prisma/client";
+import _ from "lodash";
+
+const prisma = new PrismaClient();
+const directoryPath = path.resolve(import.meta.dirname, "import-data");
+const allItems = processJsonFiles(directoryPath);
+
+// Function to read and parse JSON files
+function readJsonFile(filePath) {
+  const fileContent = fs.readFileSync(filePath, "utf-8");
+  return JSON.parse(fileContent);
+}
+
+// Function to get all JSON files from a directory
+function getJsonFilesFromDirectory(directoryPath) {
+  return fs
+    .readdirSync(directoryPath)
+    .filter((file) => path.extname(file).toLowerCase() === ".json")
+    .map((file) => path.join(directoryPath, file));
+}
+
+// Main function to process all JSON files
+function processJsonFiles(directoryPath) {
+  const jsonFiles = getJsonFilesFromDirectory(directoryPath);
+  let allItems = [];
+
+  for (const file of jsonFiles) {
+    const items = readJsonFile(file);
+    allItems = allItems.concat(items);
+  }
+
+  return allItems;
+}
+
+async function uploadProducts(products) {
+  const uniqueProducts = _.uniqBy(products, "sku");
+  const existingProducts = await prisma.product.findMany({
+    where: {
+      sku: {
+        in: uniqueProducts.map((product) => product.sku),
+      },
+    },
+  });
+  const existingProductsBySku = _.keyBy(existingProducts, "sku");
+
+  const data = [];
+  for (const product of products) {
+    const productData = {
+      sku: product.sku,
+      title: product.title,
+      price: product.price,
+      link: product.link,
+      pricePerUnit: product.unit,
+      imageUrl: product.imgSrc,
+    };
+    if (!existingProductsBySku[product.sku]) {
+      data.push(productData);
+    }
+  }
+
+  await prisma.product.createMany({
+    data,
+  });
+}
+
+uploadProducts(allItems)
+  .then(() => {
+    console.log("Products uploaded successfully");
+  })
+  .catch((error) => {
+    console.error("Error uploading products", error);
+  });
